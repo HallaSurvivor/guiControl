cc585eb1f2881c63f99c0e25fc2322c4ad6bc6da
Chris Grossack <HallaSurvivor@gmail.com>
2018-04-10 01:06:35 -0400

    start working on the prompt

--- src/Main.hs
+++ src/Main.hs
@@ -5,26 +5,45 @@ import System.Console.ANSI
 import System.IO
 import Control.Monad.Trans
 
+terminalOff :: IO ()
+terminalOff = do
+  hSetEcho stdin False
+  hSetBuffering stdin  NoBuffering
+  hSetBuffering stdout NoBuffering
+  hideCursor
+
+terminalOn :: IO ()
+terminalOn = do
+  hSetEcho stdin True
+  hSetBuffering stdin  LineBuffering
+  hSetBuffering stdout LineBuffering
+  clearScreen
+  setCursorPosition 0 0
+  showCursor
+
+
+normalMode :: W ()
+normalMode = do
+  liftIO $ terminalOff
+  loop
+
+commandMode :: W ()
+commandMode = do
+  liftIO $ putStr ":"
+  liftIO $ terminalOn
+  cmd <- liftIO $ getLine
+  runCmd cmd
+
+runCmd :: String -> W ()
+runCmd = undefined
+
 loop :: W ()
 loop = do
   k <- liftIO $ getChar
-  sendKey (Key k)
+  case k of
+    ':' -> commandMode
+    _   -> sendKey (Key k)
   loop
 
 main :: IO ()
-main = do
-  initialize
-  runW loop
-  exit
-
-    where
-      initialize = do
-        hSetEcho stdin False
-        hSetBuffering stdin NoBuffering
-        hSetBuffering stdout NoBuffering
-        hideCursor
-
-      exit = do
-        clearScreen
-        setCursorPosition 0 0
-        showCursor
+main = runW normalMode
